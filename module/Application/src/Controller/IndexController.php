f5654e48d7255cc84a05dd3fd555a0b736cac320
codeapply <me@codeapply.com>
2017-10-22 18:58:57 +0200

    application module

--- module/Application/src/Controller/IndexController.php
+++ module/Application/src/Controller/IndexController.php
@@ -14,6 +14,7 @@ class IndexController extends AbstractActionController
 {
     public function indexAction()
     {
-        return new ViewModel();
+        return new ViewModel();    
+          'name' => 'Materials manager'
     }
 }
